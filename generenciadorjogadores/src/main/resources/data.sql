3824aae68683cc996d50df97c9801d98563cd703
joaoGFG <joaogabrielfg10@gmail.com>
2025-11-06 09:14:09 -0300

    feat: data insert e validation messages

---
+++ generenciadorjogadores/src/main/resources/data.sql
@@ -0,0 +1,15 @@
+-- Inserindo dados iniciais para Times
+INSERT INTO time (nome) VALUES ('São Paulo');
+
+-- Inserindo dados iniciais para Jogadores do São Paulo + Kevin De Bruyne
+INSERT INTO jogador (nome, idade, posicao, time_id) VALUES ('Rafael', 34, 'Goleiro', 1);
+INSERT INTO jogador (nome, idade, posicao, time_id) VALUES ('Igor Vinícius', 26, 'Lateral-direito', 1);
+INSERT INTO jogador (nome, idade, posicao, time_id) VALUES ('Arboleda', 32, 'Zagueiro', 1);
+INSERT INTO jogador (nome, idade, posicao, time_id) VALUES ('Alan Franco', 27, 'Zagueiro', 1);
+INSERT INTO jogador (nome, idade, posicao, time_id) VALUES ('Welington', 23, 'Lateral-esquerdo', 1);
+INSERT INTO jogador (nome, idade, posicao, time_id) VALUES ('Pablo Maia', 22, 'Volante', 1);
+INSERT INTO jogador (nome, idade, posicao, time_id) VALUES ('Luiz Gustavo', 37, 'Volante', 1);
+INSERT INTO jogador (nome, idade, posicao, time_id) VALUES ('Lucas Moura', 32, 'Meio-campo', 1);
+INSERT INTO jogador (nome, idade, posicao, time_id) VALUES ('Kevin De Bruyne', 33, 'Meio-campo', 1);
+INSERT INTO jogador (nome, idade, posicao, time_id) VALUES ('Calleri', 30, 'Atacante', 1);
+INSERT INTO jogador (nome, idade, posicao, time_id) VALUES ('Luciano', 31, 'Atacante', 1);
